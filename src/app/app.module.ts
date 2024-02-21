e2ac4f34ce9417897a2415a5435d98aeaa3ffd8d
Thomas Reynoso <thomi.reynoso@hotmail.com>
2024-02-21 18:02:56 +0100

    Estructura de proyecto inicial (#1)
    * add views structure
    
    * get all heroes api call on service
    
    * first version of list
    
    * add angular material
    
    * add navigation to edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesListComponent } from './views/heroes-list/heroes-list.component';
+import { EditNewHeroeComponent } from './views/edit-new-heroe/edit-new-heroe.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesListComponent,
+    EditNewHeroeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/heroes-list/heroes-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Hero } from 'src/app/models/hero';
+import { HeroService } from 'src/app/services/hero.service';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './heroes-list.component.html',
+  styleUrls: ['./heroes-list.component.scss']
+})
+export class HeroesListComponent implements OnInit {
+
+  constructor(private heroService: HeroService) { }
+
+  public heroesListObs$?: Observable<Hero[]>;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes() {
+    this.heroesListObs$ = this.heroService.getHeroes();
+  }
+
+}
